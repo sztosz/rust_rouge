ef85bed4a99cf2360b04fd0952104dcf79819134
Bartosz Nowak <sztosz@gmail.com>
2020-01-18 22:36:22 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,64 @@
 rltk::add_wasm_support!();
-use rltk::{Rltk, GameState, Console};
+use rltk::{Console, GameState, Rltk, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use std::cmp::{max, min};
+mod components;
+use components::{LeftMover, Position, Renderable};
+mod systems;
+use systems::LeftWalker;
 
-struct State {}
+#[macro_use]
+extern crate specs_derive;
+
+struct State {
+    ecs: World,
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "RLTK ROUGE");
+        self.run_systems();
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
 fn main() {
     let context = Rltk::init_simple8x8(80, 50, "RLTK Rouge", "resources");
-    let games_state = State{};
-    rltk::main_loop(context, games_state)
+    let mut gs = State { ecs: World::new() };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(LeftMover {})
+            .build();
+    }
+    rltk::main_loop(context, gs)
 }
---
+++ src/systems/mod.rs
@@ -0,0 +1,18 @@
+use specs::prelude::*;
+
+use super::components::{LeftMover, Position};
+
+pub struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+  type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+  fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+    for (_left, pos) in (&lefty, &mut pos).join() {
+      pos.x -= 1;
+      if pos.x < 0 {
+        pos.x = 79;
+      }
+    }
+  }
+}
